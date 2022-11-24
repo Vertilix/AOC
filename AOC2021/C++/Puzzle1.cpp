bcb4a878b9a5576e59ab84e94e431e7c89c5be5d
VNS <96177454+Vertilix@users.noreply.github.com>
2022-11-24 13:09:03 +0100

    edited to show how many times it increased

--- AOC2021/C++/Puzzle1.cpp
+++ AOC2021/C++/Puzzle1.cpp
@@ -5,10 +5,11 @@
 using namespace std;
 
 int main() {
-    ifstream input("input.txt");
+    ifstream input("input1.txt");
     string result;
     vector<int>results;
-
+    int increase = 0;
+    
     while (getline(input, result)) {
         int i = stoi(result);
         results.push_back(i);
@@ -18,11 +19,11 @@ int main() {
     cout << results[0] << "N/A - no previous measurement" << endl;
     for (int i=1; i<results.size(); i++) {
         if (results[i] > results[i-1]){
-            cout << results[i] << "(increased)" << endl;
+            increase++;
+            cout << results[i] << " (increased) " << increase << endl;
         }else{
             cout << results[i] << "(decreased)" << endl;
         }
     }
-
     return 0;
 }
