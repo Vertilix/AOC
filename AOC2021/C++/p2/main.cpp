874d3f492a1cb09af2ca18cdaf42f86928ad0197
VNS <96177454+Vertilix@users.noreply.github.com>
2022-11-24 21:13:27 +0100

    P2 finished

--- AOC2021/C++/p2/main.cpp
+++ AOC2021/C++/p2/main.cpp
@@ -7,22 +7,21 @@ using namespace std;
 int main(){
     ifstream input("input.txt");
 
-    int horizontal;
-    int depth;
-
+    int horizontal = 0;
+    int depth = 0;
     string line;
 
     while(getline(input, line)){
-        for(int i = 0; i<9; i++){
+        for(int i = 0; i<=10; i++){
             if (line == "forward "+ to_string(i)){
-                horizontal += i;
-                cout << line << endl;
+                horizontal +=  i;
+                cout << line << horizontal << endl;
             }else if (line == "up "+ to_string(i)){
-                depth += i;
-                cout << line << endl;
-            }else if (line == "down "+ to_string(i)){
                 depth -= i;
-                cout << line << endl;
+                cout << line << depth << endl;
+            }else if (line == "down "+ to_string(i)){
+                depth += i;
+                cout << line << depth << endl;
             }
         }
     }
